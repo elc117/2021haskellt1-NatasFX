d9a372c75c1c3ead953c84d8fd663b16d930eaa5
NatasFX <nataschmitt02@hotmail.com>
2021-06-02 18:33:36 -0300

    fixed banding issue; tested some other things

--- mandelbrot_POC.py
+++ mandelbrot_POC.py
@@ -3,33 +3,54 @@ from PIL import Image
 from datetime import datetime
 from multiprocessing import Process, Pipe
 
+r = [ 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 250, 245, 240, 235, 230, 225, 220, 215, 210, 205, 200, 195, 190, 185, 180, 175, 170, 165, 160, 155, 150, 145, 140, 135, 130, 125, 120, 115, 110, 105, 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255 ]
+g = [ 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 250, 245, 240, 235, 230, 225, 220, 215, 210, 205, 200, 195, 190, 185, 180, 175, 170, 165, 160, 155, 150, 145, 140, 135, 130, 125, 120, 115, 110, 105, 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+b = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 250, 245, 240, 235, 230, 225, 220, 215, 210, 205, 200, 195, 190, 185, 180, 175, 170, 165, 160, 155, 150, 145, 140, 135, 130, 125, 120, 115, 110, 105, 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5 ]
+	
+
+
 def mandelbrotColor(x, y, iterations):
     cx = x
     cy = y
 
     for i in range(1, iterations+1):
 
-        nx = x*x - y*y + cx
+        xsqrd = x*x
+        ysqrd = y*y
+
+        nx = xsqrd - ysqrd + cx
         ny = 2*x*y + cy
 
         y = ny
         x = nx
 
-        if (x*x + y*y > 4): break
+        if ((xsqrd + ysqrd)/2 > 4.5): break
 
     if (i == iterations): return [0,0,0]
+    if i == 1: return [255,255,255]
 
-    color = iterations/i*30
+    peso = ((xsqrd + ysqrd)/2 - 4.5)/25
+    peso = 1 if peso > 1 else peso
 
-    color = 255 if color > 255 else color
+    colorR = iterations/(i+1-peso)*20
 
-    return [color, color, color]
+    colorR = 255 if colorR > 255 else colorR
+
+    #colorR = r[int(iterations/(i+1-peso)*20)%len(r)]
+    #colorG = g[int(iterations/(i+1-peso)*20)%len(g)]
+    #colorB = b[int(iterations/(i+1-peso)*20)%len(b)]
+
+    #colorG = 255-(iterations/i)%255*6
+
+    #colorB = 255-(iterations/i)%255*7
+
+    return [colorR, colorR, colorR]
 
 def calcPixel(data, width, height, startingY, endY, size, iterations, offsetX, offsetY, pipe):
     
     for x in range(int(2*width)):
         for y in range(startingY, endY):
-            data[y-startingY][x] = mandelbrotColor((x-width)/(size)-offsetX, (y-height)/(size)-offsetY, iterations)
+            data[y-startingY][x] = mandelbrotColor((x-width)/(size)+offsetX, (y-height)/(size)+offsetY, iterations)
 
     pipe.send((data, startingY, endY))
     pipe.close()
@@ -38,29 +59,34 @@ def createMandelbrot(WIDTH, HEIGHT, NUMBERTHREADS, ITERATIONS, SCALE):
 
     heights = []
     for i in range(NUMBERTHREADS):
-        heights.append((i*HEIGHT/NUMBERTHREADS, (i+1)*HEIGHT/NUMBERTHREADS))
-
-    data = np.zeros((HEIGHT,WIDTH,3), dtype=np.uint8)
+        heights.append(
+            (int(i*HEIGHT/NUMBERTHREADS),
+            int((i+1)*HEIGHT/NUMBERTHREADS))
+        )
 
     threads = []
     for begin, end in heights:
         rcv, snd = Pipe()
         arg = [
-            np.zeros((int(end)-int(begin),WIDTH,3), dtype=np.uint8),
+            np.zeros((end-begin,WIDTH,3), dtype=np.uint8),
             WIDTH/2,
             HEIGHT/2,
-            int(begin),
-            int(end),
+            begin,
+            end,
             SCALE,
             ITERATIONS,
-            .865000025,
-            .245,
+            #0.36024044343761436323612524444954530848260780795858575048837581474019534605,
+            #-0.64131306106480317486037501517930206657949495228230525955617754306444857417,
+            #-.865000025,
+            -.73,
+            -.245,
             snd
         ]
         process = Process(target=calcPixel, args=arg)
         process.start()
         threads.append((process, rcv))
     
+    data = np.zeros((HEIGHT,WIDTH,3), dtype=np.uint8)
     for process, conn in threads:
         res = conn.recv()
         data[res[1]:res[2], 0:WIDTH] = res[0]
@@ -72,25 +98,30 @@ if __name__ == '__main__':
 
     elapsed = datetime.now()
 
-    NUMBERTHREADS = 16
-    WIDTH  = 4320
-    HEIGHT = 7680
+    NUMBERTHREADS = 8
+    WIDTH  = 800
+    HEIGHT = 600
 
     numberOfFrames = 1
 
     #ffmpeg -framerate 24 -i %d.png -c:v libx264 -pix_fmt yuv420p output.mp4
 
+    finalZoom = 20000
+
     scales = []
     for i in range(1, numberOfFrames+1):
-        scales.append((i, int((200000/numberOfFrames)*i)))
+        scales.append((i, int((finalZoom/numberOfFrames)*i)))
+
+    #aumentar 1 iteracao a cada 585937 de zoom
 
     for i, scale in scales:
         beginTime = datetime.now()
 
-        img = createMandelbrot(WIDTH, HEIGHT, NUMBERTHREADS, 255, scale)
+        img = createMandelbrot(WIDTH, HEIGHT, NUMBERTHREADS, 280, scale)
+        img.show()
+        #break
 
         img.save(f"./images/{i}.png", "PNG")
         print(f'\rComputed {i+1} frame - {NUMBERTHREADS} threads - time: {(datetime.now()-beginTime).total_seconds():.1f}s', end='')
-
 
     print(f'\nRendered {i} frames - time elapsed: {(datetime.now()-elapsed).total_seconds():.0f}s')
