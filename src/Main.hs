7bcdb5f77b08c70e26d72466a4f0af2249dc8258
NatasFX <nataschmitt02@hotmail.com>
2021-06-07 11:45:40 -0300

    Haskell Mandelbrot v1.1

--- src/Main.hs
+++ src/Main.hs
@@ -7,31 +7,38 @@
 import Codec.Picture
 type Complex = (Double, Double)
 
-width, height :: Int
-width = 960
-height = 720
 
-coordY, coordX :: Double
-coordX = -0.5
+width, height, maxIter :: Int
+width = 1080
+height = 1920
+maxIter = 255
+
+coordY, coordX, scale :: Double
+scale = 600000000
+coordX = -1.940157358
 coordY = 0
 
+filterPallete :: [(Double, Double, Double)] -> [(Pixel8, Pixel8, Pixel8)]
+filterPallete = map (\(r,g,b) -> (round r, round g, round b)) 
+
+pallete, palleteLaranja :: [(Pixel8, Pixel8, Pixel8)]
+pallete = filterPallete (zip3 [0..254.0] [0,0.72..184] (replicate 254 0.0))
+
+palleteLaranja = cycle (pallete ++ reverse pallete)
 
 
-calculatePoint :: Complex -> Complex -> Int -> Int -> Int
-calculatePoint (cx,cy) (zx,zy) iter maxIter = if iter < maxIter && (zx + zy) < 4 then calculatePoint (cx,cy) (zx*zx - zy*zy + cx , 2*zx*zy + cy) (iter+1) maxIter else iter
+calcPoint :: Complex -> Complex -> Int  -> Int
+calcPoint (cx,cy) (zx,zy) iter = if iter < maxIter && (zx + zy)/2 < 4 then calcPoint (cx,cy) (zx*zx - zy*zy + cx , 2*zx*zy + cy) (iter+1) else iter
 
-colorFromIter :: Int -> [Pixel8]
-colorFromIter iter = [fromIntegral n,fromIntegral n,fromIntegral n]
-    where n = if iter == 256 then 0 else mod (iter*3) 255
-
-genPixel :: Double -> Double -> [Pixel8]
-genPixel x y = colorFromIter (calculatePoint ((x-fromIntegral width/2)/size+coordX, (y-fromIntegral height/2)/size+coordY) (0,0) 0 255)
-  where size = 300
-
-toRGB :: Int -> Int -> PixelRGB8
-toRGB x y = PixelRGB8 (head color) (fromIntegral (color!!1)) (fromIntegral (last color))
-    where color = genPixel (fromIntegral x) (fromIntegral y)
+colorFromIter :: Int -> PixelRGB8
+colorFromIter iter = (\(r,g,b) -> PixelRGB8 r g b) n
+    where n = palleteLaranja!!mod (iter*4) (length pallete*2)
 
 main :: IO ()
-main = writeBitmap "./mandelbrot.bmp" $ generateImage toRGB width height
-
+main = 
+  writeBitmap "./mandelbrot.bmp" $ generateImage genPixel width height
+    where genPixel x y = colorFromIter (calcPoint (xPos, yPos) (0,0) 0)
+            where xPos = (x'-w/2)/scale+coordX
+                  yPos = (y'-h/2)/scale+coordY
+                  (w,h) = (fromIntegral width, fromIntegral height)
+                  (x', y') = (fromIntegral x, fromIntegral y)
